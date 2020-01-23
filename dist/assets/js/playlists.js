f857eaaf0e93d0f33440089bdcfa3547836374e2
Bianca Karitas jensen <5884546@rts-365.dk>
2020-01-23 12:31:35 +0100

    Update

--- dist/assets/js/playlists.js
+++ dist/assets/js/playlists.js
@@ -1,36 +1,4 @@
 "use strict";
 
-////////////////////////HENTER NY TOKEN//////////////////////////
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("https://api.spotify.com/v1/browse/featured-playlists", {
-    method: "GET",
-    headers: {
-      "Authorization": 'Bearer ' + sessionStorage.token
-    }
-  }).then(function (response) {
-    return response.json();
-  }).then(function (result) {
-    if (result.error) {
-      getToken();
-    } else {
-      //console.log(result)
-      //console.log(result.playlists) 
-      result.playlists.items.forEach(function (element) {
-        //console.log(element) 
-        console.log(element); // Template
-
-        var containerImage = document.querySelector(".swiper-wrapper");
-        var templateImage = document.getElementById("playlists-images");
-        var containerText = document.querySelector(".playlists__headerTitle");
-        var templateText = document.getElementById("playlists-playlistsTitle");
-        var cloneImage = templateImage.content.cloneNode(true);
-        var cloneText = templateText.content.cloneNode(true);
-        cloneImage.querySelector(".swiper-slide").style = "background-image:url(" + element.images[0].url + ")";
-        cloneText.querySelector(".playlists__playlistsText").innerHTML = element.name; // Tilføjer clone
-
-        containerImage.appendChild(cloneImage);
-        containerText.appendChild(cloneText);
-      });
-    }
-  });
+document.addEventListener("DOMContentLoaded", function () {///////////////////////////// VISER TRACKS FRA DET VALGTE PLAYLISTE //////////////////// 
 });
--- src/js/playlists.js
+++ src/js/playlists.js
@@ -1,40 +1,7 @@
-
-////////////////////////HENTER NY TOKEN//////////////////////////
 document.addEventListener("DOMContentLoaded", () =>{
 
-	fetch("https://api.spotify.com/v1/browse/featured-playlists", {
-		method: "GET",
-		headers: {
-			"Authorization": 'Bearer ' + sessionStorage.token
-		}
-	})
-	.then((response) => response.json())
-	.then((result) => {
-		if (result.error){
-			getToken();
-		}else{
-			//console.log(result)
-			//console.log(result.playlists) 
-			result.playlists.items.forEach(element => {
-				//console.log(element) 
-				console.log(element)
-				
-				// Template
-				const containerImage = document.querySelector(".swiper-wrapper");
-				const templateImage = document.getElementById("playlists-images");
-				const containerText = document.querySelector(".playlists__headerTitle");
-				const templateText = document.getElementById("playlists-playlistsTitle");
+	///////////////////////////// VISER TRACKS FRA DET VALGTE PLAYLISTE //////////////////// 
 
-				const cloneImage = templateImage.content.cloneNode(true);
-				const cloneText = templateText.content.cloneNode(true);
+	
 
-				cloneImage.querySelector(".swiper-slide").style = "background-image:url("+element.images[0].url+")";
-				cloneText.querySelector(".playlists__playlistsText").innerHTML = element.name;
-				
-				// Tilføjer clone
-				containerImage.appendChild(cloneImage);
-				containerText.appendChild(cloneText);
-			}); 
-		}
-	})
 });
