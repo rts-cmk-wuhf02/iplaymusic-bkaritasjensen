b10ec82fd6dd1fdce88e595f6da759bbba60d11e
Bianca Karitas jensen <5884546@rts-365.dk>
2020-01-28 12:12:18 +0100

    Upadte

--- dist/assets/js/categories.js
+++ dist/assets/js/categories.js
@@ -2,8 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   //////////////////////////////// CATEGORI //////////////////////////////////
-  var categoriesURL = "https://api.spotify.com/v1/browse/categories";
-  var subcategoriesURL = "https://api.spotify.com/v1/browse/categories/workout/playlists";
+  var categoriesURL = "https://api.spotify.com/v1/browse/categories?country=DK";
   fetch(categoriesURL, {
     method: "GET",
     headers: {
@@ -12,10 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
   }).then(function (response) {
     return response.json();
   }).then(function (result) {
-    if (result.error) {
+    if (result.error && result.error.status === 401) {
+      //console.log(result)
       getToken();
     } else {
-      console.log(result);
+      //console.log(result)
       result.categories.items.forEach(function (element) {
         //console.log(element)
         //Template
@@ -24,30 +24,57 @@ document.addEventListener("DOMContentLoaded", function () {
         var cloneCategories = templateCategories.content.cloneNode(true);
         cloneCategories.querySelector(".categories__title").innerText = element.name; // Tilføjer clone
 
-        containerCategories.appendChild(cloneCategories);
-      });
-    }
-  });
-  fetch(subcategoriesURL, {
-    method: "GET",
-    headers: {
-      "Authorization": 'Bearer ' + sessionStorage.token
-    }
-  }).then(function (response) {
-    return response.json();
-  }).then(function (result) {
-    console.log(result.playlists.items[0].name);
+        containerCategories.appendChild(cloneCategories); ////////////////////////// TILFØJER SUBCATEGORIES /////////////////////////////////////
 
-    if (result.error) {
-      getToken();
-    } else {
-      result.playlists.items.forEach(function (element) {
-        var containerSubcategories = document.querySelector(".subcategories__cardList");
-        var templateSubcategories = document.getElementById("subcategories-template");
-        var cloneSubcategories = templateSubcategories.content.cloneNode(true);
-        cloneSubcategories.querySelector(".subcategories__text").innerText = element.name;
-        containerSubcategories.appendChild(cloneSubcategories);
+        var subCategoriesID = element.id; //console.log(subCategoriesID)
+
+        fetch("https://api.spotify.com/v1/browse/categories/".concat(subCategoriesID, "/playlists"), {
+          method: "GET",
+          headers: {
+            "Authorization": 'Bearer ' + sessionStorage.token
+          }
+        }).then(function (response) {
+          return response.json();
+        }).then(function (result) {
+          if (result.error && result.error.status === 401) {
+            getToken();
+          } else {
+            console.log(result);
+            result.playlists.items.forEach(function (element) {
+              //console.log(element)
+              var containerSubcategories = document.querySelector(".subcategories__cardList");
+              var templateSubcategories = document.getElementById("subcategories-template");
+              var cloneSubcategories = templateSubcategories.content.cloneNode(true);
+              cloneSubcategories.querySelector(".subcategories__text").innerText = element.name;
+              containerSubcategories.appendChild(cloneSubcategories);
+            });
+          }
+        });
       });
     }
   });
+  /* fetch(subcategoriesURL, {
+  	method: "GET",
+  	headers: {
+  		"Authorization": 'Bearer ' + sessionStorage.token
+  	}
+  })
+  .then((response) => response.json())
+  .then((result) => {
+  	//console.log(result)
+  	if (result.error){
+  		getToken();
+  	}else{
+  		result.playlists.items.forEach(element => {
+  			const containerSubcategories = document.querySelector(".subcategories__cardList");
+  			const templateSubcategories = document.getElementById("subcategories-template");
+  			const cloneSubcategories = templateSubcategories.content.cloneNode(true);
+  			
+  			cloneSubcategories.querySelector(".subcategories__text").innerText = element.name;
+  			
+  			containerSubcategories.appendChild(cloneSubcategories); 
+  		})
+  	}
+  })
+  */
 });
--- src/js/categories.js
+++ src/js/categories.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", () =>{
 	
 	//////////////////////////////// CATEGORI //////////////////////////////////
-	let categoriesURL = "https://api.spotify.com/v1/browse/categories";
-	var subcategoriesURL = `https://api.spotify.com/v1/browse/categories/workout/playlists`;
+	let categoriesURL = "https://api.spotify.com/v1/browse/categories?country=DK";
 	
 	fetch(categoriesURL, {
 		method: "GET",
@@ -12,28 +11,62 @@ document.addEventListener("DOMContentLoaded", () =>{
 	})
 	.then((response) => response.json())
 	.then((result) => {
-		if (result.error){
+		if (result.error && result.error.status === 401){
+			//console.log(result)
 			getToken();
 		}else{
-			console.log(result)
+			//console.log(result)
 			result.categories.items.forEach(element => {
 				//console.log(element)
 				//Template
 				const containerCategories = document.getElementById("dropdown");
 				const templateCategories = document.getElementById("categories-template");
 				const cloneCategories = templateCategories.content.cloneNode(true);
-
+				
 				cloneCategories.querySelector(".categories__title").innerText = element.name;
-				 
+				
 				// Tilføjer clone
 				containerCategories.appendChild(cloneCategories);
+
+
+				////////////////////////// TILFØJER SUBCATEGORIES /////////////////////////////////////
+				var subCategoriesID = element.id;
+				//console.log(subCategoriesID)
+
+				fetch(`https://api.spotify.com/v1/browse/categories/${subCategoriesID}/playlists`, {
+					method: "GET",
+					headers: {
+						"Authorization": 'Bearer ' + sessionStorage.token
+					}
+				})
+				.then((response) => response.json())
+				.then((result) => {
+					if (result.error && result.error.status === 401){
+						getToken();
+					}else{
+						console.log(result)
+						result.playlists.items.forEach(element => {
+							//console.log(element)
+							const containerSubcategories = document.querySelector(".subcategories__cardList");
+							const templateSubcategories = document.getElementById("subcategories-template");
+							const cloneSubcategories = templateSubcategories.content.cloneNode(true);
+				
+							cloneSubcategories.querySelector(".subcategories__text").innerText = element.name;
+				
+							containerSubcategories.appendChild(cloneSubcategories); 
+
+						});
+					}
+				})
+
 			});
-		
-		
+			
+			
 		}
 	});
 	
-	fetch(subcategoriesURL, {
+	
+	/* fetch(subcategoriesURL, {
 		method: "GET",
 		headers: {
 			"Authorization": 'Bearer ' + sessionStorage.token
@@ -41,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () =>{
 	})
 	.then((response) => response.json())
 	.then((result) => {
-		console.log(result.playlists.items[0].name)
+		//console.log(result)
 		if (result.error){
 			getToken();
 		}else{
@@ -52,10 +85,10 @@ document.addEventListener("DOMContentLoaded", () =>{
 				
 				cloneSubcategories.querySelector(".subcategories__text").innerText = element.name;
 				
-				containerSubcategories.appendChild(cloneSubcategories);
+				containerSubcategories.appendChild(cloneSubcategories); 
 			})
 		}
 	})
-
+ */
 
 });
