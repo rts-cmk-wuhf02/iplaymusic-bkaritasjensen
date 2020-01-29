76179eb65fe80d38769aaa9a003f129e711b4da4
Bianca Karitas jensen <5884546@rts-365.dk>
2020-01-29 17:18:24 +0100

    Update

--- dist/assets/js/dropdown.js
+++ dist/assets/js/dropdown.js
@@ -7,6 +7,7 @@ function myFunction() {
 
 window.onclick = function (e) {
   if (!e.target.matches('.dropbtn')) {
+    console.log(e);
     var myDropdown = document.getElementById("myDropdown");
 
     if (myDropdown.classList.contains('show')) {
--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -5,9 +5,11 @@ function myFunction() {
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(e) {
 	if (!e.target.matches('.dropbtn')) {
+		console.log(e)
 	var myDropdown = document.getElementById("myDropdown");
 	  if (myDropdown.classList.contains('show')) {
 		myDropdown.classList.remove('show');
 	  }
 	}
-  }
+}
+  
